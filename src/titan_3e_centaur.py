cdbf7855c9ad9e9995e988170580e228d680cdb8
kwan3217 <kwan3217@gmail.com>
2025-01-21 23:38:11 -0700

    Update telemetry plotting to test both TC-7 and TC-6.

--- src/titan_3e_centaur.py
+++ src/titan_3e_centaur.py
@@ -2,11 +2,12 @@
 Model of a Titan-III/Centaur (Titan 3E)
 """
 import numpy as np
+from matplotlib import pyplot as plt
 
 from rocket_sim.srm import SRM
 from rocket_sim.universe import ZeroGRange
 from rocket_sim.vehicle import Stage, kg_per_lbm, Vehicle, Engine, N_per_lbf, g0
-from test_rocket_sim.test_vehicle import tlm, plot_tlm
+from test_rocket_sim.test_vehicle import tlm
 from voyager import Voyager
 
 
@@ -65,17 +66,45 @@ class Titan3E(Vehicle):
                         engines=[(self.srmL,0),(self.srmR,1)],extras=[tlm])
 
 
+def plot_tlm(vehicle:Vehicle,tc_id:int):
+    ts=np.array([t for t,y,mass,thr_mag in vehicle.tlm])
+    states=np.array([y for t,y,mass,thr_mag in vehicle.tlm])
+    masses=np.array([mass for t,y,mass,thr_mag in vehicle.tlm])
+    thr_mags=np.array([thr_mag for t,y,mass,thr_mag in vehicle.tlm])
+    plt.figure("Titan3E telemetry")
+    plt.subplot(2,2,1)
+    plt.title("Mass")
+    plt.plot(masses,label=f'mass {tc_id}')
+    plt.legend()
+    plt.subplot(2,2,2)
+    plt.title("acc")
+    plt.plot(thr_mags/masses/9.80665,label=f'acc {tc_id}')
+    plt.legend()
+    plt.subplot(2,2,3)
+    plt.title("v")
+    plt.plot(states[:,5],label=f'vz {tc_id}')
+    plt.legend()
+    plt.subplot(2,2,4)
+    plt.title("pos")
+    plt.plot(states[:,2],label='rz {tc_id}')
+    plt.legend()
+    plt.pause(0.1)
+
+
+
 def main():
-    titan3E=Titan3E(tc_id=6)
-    print(titan3E.stages)
-    print(titan3E.engines)
-    if False:
-        stand=TestStand(vehicles=[titan3E],fps=10)
-        stand.runto(t1=130.0)
-    else:
-        range=ZeroGRange(vehicles=[titan3E],fps=10)
-        range.runto(t1=130.0)
-    plot_tlm(titan3E)
+    for tc_id in (6,7):
+        titan3E=Titan3E(tc_id=tc_id)
+        print(titan3E.stages)
+        print(titan3E.engines)
+        if False:
+            stand=TestStand(vehicles=[titan3E],fps=10)
+            stand.runto(t1=130.0)
+        else:
+            range=ZeroGRange(vehicles=[titan3E],fps=10)
+            range.runto(t1=130.0)
+        plot_tlm(titan3E,tc_id=tc_id)
+    plt.show()
 
 
 if __name__=="__main__":
