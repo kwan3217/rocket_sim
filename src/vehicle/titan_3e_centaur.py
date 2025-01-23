9487cde340d03a3f18b70fb6d354015bea04b874
kwan3217 <kwan3217@gmail.com>
2025-01-23 01:20:31 -0700

    Initial implementation of pitch program

--- src/vehicle/titan_3e_centaur.py
+++ src/vehicle/titan_3e_centaur.py
@@ -3,9 +3,14 @@ Model of a Titan-III/Centaur (Titan 3E)
 """
 import numpy as np
 
+from guidance.pitch_program import pitch_program
+from rocket_sim.drag import INCHES, f_drag, mach_drag
+from rocket_sim.gravity import SpiceTwoBody, SpiceJ2, SpiceThirdBody
+from rocket_sim.planet import Earth
 from rocket_sim.srm import SRM
+from rocket_sim.universe import Universe
 from rocket_sim.vehicle import Stage, kg_per_lbm, Vehicle, Engine, N_per_lbf, g0
-from vehicle.voyager import Voyager
+from vehicle.voyager import Voyager, init_spice, voyager_et0
 
 
 class Titan3E(Vehicle):
@@ -61,5 +66,49 @@ class Titan3E(Vehicle):
         super().__init__(stages=[self.srbL,self.srbR,self.stage1,self.stage2,
                                 self.interstage_and_shroud,self.centaur,self.pm,self.sc],
                         engines=[(self.srmL,0),(self.srmR,1)])
+
+
+def ascend(*,vgr_id:int):
+    earth=Earth()
+    pad41_lat= 28.583468 # deg, From Google Earth, so on WGS-84
+    pad41_lon=-80.582876
+    pad41_alt=0 # Hashtag Florida
+    y0=earth.launchpad(lat=pad41_lat,lon=pad41_lon,alt=pad41_alt,deg=True,et=voyager_et0[vgr_id])
+    titan3E = Titan3E(tc_id=vgr_id+5)
+    # Flight azimuth from TC-6 report section IV
+    titan3E.guide = pitch_program(planet=earth, y0=y0, azimuth=90.0, deg=True,
+                                  t0=0.0, pitch0=92.0,
+                                  tdpitch=[( 10.0, -1.17),
+                                           ( 20.0, -0.53),
+                                           ( 30.0, -0.73),
+                                           ( 62.0, -0.63),
+                                           ( 75.0, -0.52),
+                                           ( 95.0, -0.38),
+                                           (114.0,  0.00),
+                                           (130.0, -0.75),
+                                           (140.0, -0.08)])
+    print(titan3E.stages)
+    print(titan3E.engines)
+    earth_twobody = SpiceTwoBody(spiceid=399)
+    earth_j2 = SpiceJ2(spiceid=399)
+    moon = SpiceThirdBody(spice_id_center=399, spice_id_body=301, et0=voyager_et0[vgr_id])
+    sun = SpiceThirdBody(spice_id_center=399, spice_id_body=10, et0=voyager_et0[vgr_id])
+    Sref=np.pi*(7*12*INCHES)**2+2*np.pi*(60*INCHES)**2
+    drag=f_drag(planet=earth, clcd=mach_drag(), Sref=Sref)
+    sim = Universe(vehicles=[titan3E],
+                   accs=[earth_twobody, earth_j2, moon, sun],
+                   forces=[],
+                   t0=0, y0s=[y0], fps=10)
+    sim.runto(t1=130.0)
+    #plot_tlm(titan3E, tc_id=titan3E.tc_id)
+
+
+def main():
+    init_spice()
+    ascend(vgr_id=1)
+
+
+if __name__=="__main__":
+    main()
 
 
